f1b349f5309c00922772dc0d05a4275e688c5668
Michele Riva <ciao@micheleriva.it>
2022-06-28 15:19:05 +0200

    feat: adds title to homepage

--- nextjs-app/pages/index.js
+++ nextjs-app/pages/index.js
@@ -25,23 +25,26 @@ export async function getServerSideProps() {
 
 export default function Home(props) {
   return (
-    <div className='py-36 grid grid-cols-4 gap-4'>
-      {
-        props.products.map((p) => (
-          <div className='relative bg-slate-200 rounded-md' key={p.id}>
-            <img src={p.thumbnail} className='w-full h-52 object-cover rounded-t-md' />
-            <div className='p-4'>
-              <h2 className='font-bold text-lg'> {p.title} </h2>
-              <p className='text-md h-28'> {p.description} </p>
+    <>
+      <h1 className='w-full text-center text-4xl font-bold py-36'> My store </h1>
+      <div className='pb-36 grid grid-cols-4 gap-4'>
+        {
+          props.products.map((p) => (
+            <div className='relative bg-slate-200 rounded-md' key={p.id}>
+              <img src={p.thumbnail} className='w-full h-52 object-cover rounded-t-md' />
+              <div className='p-4'>
+                <h2 className='font-bold text-lg'> {p.title} </h2>
+                <p className='text-md h-28'> {p.description} </p>
 
-              <div className='flex justify-between'>
-                <p className='text-emerald-600 font-bold'> {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(p.price)} </p>
-                <a href={`/products/${p.id}`} className='bg-emerald-500 text-white rounded-md px-2 py-1'>Details</a>
+                <div className='flex justify-between'>
+                  <p className='text-emerald-600 font-bold'> {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(p.price)} </p>
+                  <a href={`/products/${p.id}`} className='bg-emerald-500 text-white rounded-md px-2 py-1'>Details</a>
+                </div>
               </div>
             </div>
-          </div>
-        ))
-      }
-    </div>
+          ))
+        }
+      </div>
+    </>
   )
 }
